24f94432a3b2532d4717ba7cb3e17f5ec39f0673
Brian Simpson <50429915+bmsimp@users.noreply.github.com>
2025-04-01 09:58:25 -0500

    Update Convert Unifi Javascript Model Output to JSON List.ps1

--- Convert Unifi Javascript Model Output to JSON List.ps1
+++ Convert Unifi Javascript Model Output to JSON List.ps1
@@ -2,11 +2,13 @@
 $jsonFilePath = "C:\path\to\your\file.json"
 $jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
 
+# Create a list to store the transformed data
+$transformedData = [System.Collections.Generic.List[PSObject]]::new()
+
 # Transform the JSON
-$transformedData = @()
 foreach ($key in $jsonContent.PSObject.Properties.Name) {
     $item = $jsonContent.$key | Add-Member -MemberType NoteProperty -Name "model" -Value $key -PassThru
-    $transformedData += $item
+    $transformedData.Add($item)
 }
 
 # Convert back to JSON and save to a new file
